b5768e8276cd2cec555c87ec1ab1771829e962c2
Daniel Schaefer <dhs@frame.work>
2023-02-10 19:08:44 +0800

    Simplify lotus HAL
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

--- src/lotus_led_hal.rs
+++ src/lotus_led_hal.rs
@@ -2,14 +2,11 @@
 
 pub extern crate rp2040_hal as hal;
 
-//#[cfg(feature = "rt")]
 extern crate cortex_m_rt;
-//#[cfg(feature = "rt")]
 pub use hal::entry;
 
 /// The linker will place this boot block at the start of our program image. We
 /// need this to help the ROM bootloader get our code up and running.
-//#[cfg(feature = "boot2")]
 #[link_section = ".boot2"]
 #[no_mangle]
 #[used]
@@ -19,75 +16,28 @@ pub use hal::pac;
 
 hal::bsp_pins!(
     /// GPIO 0 is connected to the SLEEP# pin of the EC
-    Gpio0 {
-        name: sleep,
-    },
-
-    /// GPIO 26 supports following functions:
-    ///
-    /// | Function     | Alias with applied function |
-    /// |--------------|-----------------------------|
-    /// | `SPI1 SCK`   | [crate::Gp26Spi1Sck]        |
-    /// | `UART1 CTS`  | [crate::Gp26Uart1Cts]       |
-    /// | `I2C1 SDA`   | [crate::Gp26I2C1Sda]        |
-    /// | `PWM5 A`     | [crate::Gp26Pwm5A]          |
-    /// | `PIO0`       | [crate::Gp26Pio0]           |
-    /// | `PIO1`       | [crate::Gp26Pio1]           |
+    Gpio0 { name: sleep },
+    /// GPIO 26 is connected to I2C SDA of the LED controller
     Gpio26 {
         name: gpio26,
         aliases: {
-            /// UART Function alias for pin [crate::Pins::gpio26].
-            FunctionUart: Gp26Uart1Cts,
-            /// SPI Function alias for pin [crate::Pins::gpio26].
-            FunctionSpi: Gp26Spi1Sck,
             /// I2C Function alias for pin [crate::Pins::gpio26].
-            FunctionI2C: Gp26I2C1Sda,
-            /// PWM Function alias for pin [crate::Pins::gpio26].
-            FunctionPwm: Gp26Pwm5A,
-            /// PIO0 Function alias for pin [crate::Pins::gpio26].
-            FunctionPio0: Gp26Pio0,
-            /// PIO1 Function alias for pin [crate::Pins::gpio26].
-            FunctionPio1: Gp26Pio1
+            FunctionI2C: Gp26I2C1Sda
         }
     },
-
-    /// GPIO 27 supports following functions:
-    ///
-    /// | Function     | Alias with applied function |
-    /// |--------------|-----------------------------|
-    /// | `SPI1 TX`    | [crate::Gp27Spi1Tx]         |
-    /// | `UART1 RTS`  | [crate::Gp27Uart1Rts]       |
-    /// | `I2C1 SCL`   | [crate::Gp27I2C1Scl]        |
-    /// | `PWM5 B`     | [crate::Gp27Pwm5B]          |
-    /// | `PIO0`       | [crate::Gp27Pio0]           |
-    /// | `PIO1`       | [crate::Gp27Pio1]           |
+    /// GPIO 27 is connected to I2C SCL of the LED controller
     Gpio27 {
         name: gpio27,
         aliases: {
-            /// UART Function alias for pin [crate::Pins::gpio27].
-            FunctionUart: Gp27Uart1Rts,
-            /// SPI Function alias for pin [crate::Pins::gpio27].
-            FunctionSpi: Gp27Spi1Tx,
             /// I2C Function alias for pin [crate::Pins::gpio27].
-            FunctionI2C: Gp27I2C1Scl,
-            /// PWM Function alias for pin [crate::Pins::gpio27].
-            FunctionPwm: Gp27Pwm5B,
-            /// PIO0 Function alias for pin [crate::Pins::gpio27].
-            FunctionPio0: Gp27Pio0,
-            /// PIO1 Function alias for pin [crate::Pins::gpio27].
-            FunctionPio1: Gp27Pio1
+            FunctionI2C: Gp27I2C1Scl
         }
     },
-
     /// GPIO 29 is connected to the INTB pin of the LED controller
-    Gpio28 {
-        name: intb,
-    },
-
+    Gpio28 { name: intb },
     /// GPIO 29 is connected to the SDB pin of the LED controller
-    Gpio29 {
-        name: sdb,
-    },
+    Gpio29 { name: sdb },
 );
 
+// External crystal frequency, same as Raspberry Pi Pico
 pub const XOSC_CRYSTAL_FREQ: u32 = 12_000_000;
