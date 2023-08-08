bf0dd0ff6db2ce6977654802223ad32436fbac52
Daniel Schaefer <dhs@frame.work>
2023-08-08 18:08:41 +0800

    ledmatrix_control: Use black/white API for pattern
    This way the brightness of on and off is controlled by the firmware.
    
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

--- ledmatrix_control.py
+++ ledmatrix_control.py
@@ -592,33 +592,29 @@ def set_color(color):
 
 
 def checkerboard(dev, n):
-    with serial.Serial(dev.device, 115200) as s:
-        for x in range(0, WIDTH):
-            vals = (([0xFF] * n) + ([0x00] * n)) * int(HEIGHT/2)
-            if x % (n*2) < n:
-                # Rotate once
-                vals = vals[n:] + vals[:n]
+    matrix = [[0 for _ in range(HEIGHT)] for _ in range(WIDTH)]
+    for x in range(0, WIDTH):
+        vals = (([0xFF] * n) + ([0x00] * n)) * int(HEIGHT/2)
+        if x % (n*2) < n:
+            # Rotate once
+            vals = vals[n:] + vals[:n]
 
-            send_col(s, x, vals)
-        commit_cols(s)
+        matrix[x] = vals
+    render_matrix(dev, matrix)
 
 
 def every_nth_col(dev, n):
-    with serial.Serial(dev.device, 115200) as s:
-        for x in range(0, WIDTH):
-            vals = [(0xFF if x % n == 0 else 0) for _ in range(HEIGHT)]
-
-            send_col(s, x, vals)
-        commit_cols(s)
+    matrix = [[0 for _ in range(HEIGHT)] for _ in range(WIDTH)]
+    for x in range(0, WIDTH):
+        matrix[x] = [(0xFF if x % n == 0 else 0) for _ in range(HEIGHT)]
+    render_matrix(dev, matrix)
 
 
 def every_nth_row(dev, n):
-    with serial.Serial(dev.device, 115200) as s:
-        for x in range(0, WIDTH):
-            vals = [(0xFF if y % n == 0 else 0) for y in range(HEIGHT)]
-
-            send_col(s, x, vals)
-        commit_cols(s)
+    matrix = [[0 for _ in range(HEIGHT)] for _ in range(WIDTH)]
+    for x in range(0, WIDTH):
+        matrix[x] = [(0xFF if y % n == 0 else 0) for y in range(HEIGHT)]
+    render_matrix(dev, matrix)
 
 
 def all_brightnesses(dev):
